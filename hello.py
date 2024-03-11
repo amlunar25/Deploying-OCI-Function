813de64d117ea0d4c85509ef2cd65bccd35554ba
Alexander Luna <alluna@fanatics.com>
2024-03-11 17:03:29 -0500

    Printing name variable for test

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from fdk import response
 
 def handler(ctx, data: io.BytesIO = None):
     name = "World"
+    print(name)
     try:
         body = json.loads(data.getvalue())
         name = body.get("name")
